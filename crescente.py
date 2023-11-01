cf320803c41ea86b797870c94a445902b1a1ab85
kookpa <141832427+kookpa@users.noreply.github.com>
2023-11-01 11:28:48 -0300

    Add files via upload

---
+++ crescente.py
@@ -0,0 +1,8 @@
+num1 = int(input("digite o primeiro numero:"))
+num2= int(input("digite o segundo numero:"))
+num3= int(input("digite o terceiro numero:"))
+
+lista_numero= [num1, num2, num3]
+lista_numero.sort()
+
+print("numero em ordem crescente:", lista_numero)
